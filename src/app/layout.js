f35ac722afdf9fa4a6d7737ec40757dbc02abb83
Rashaunjw <rashaunjwilliams@gmail.com>
2025-09-24 19:00:57 -0500

    minor change

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,6 +29,7 @@ export default function RootLayout({ children }) {
         <meta name="color-scheme" content="light" />
         <meta name="theme-color" content="#F4E9E1" />
         <meta name="supported-color-schemes" content="light" />
+        <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext y='.9em' font-size='90'%3E🧙‍♂️%3C/text%3E%3C/svg%3E" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/favicon.ico" />
